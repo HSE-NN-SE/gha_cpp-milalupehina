e80ff0f60717e58e71d4d149fbffa487c4dbc5ce
anton <ashtanyuk@gmail.com>
2020-11-09 11:28:21 +0300

    Tests added

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,3 +10,25 @@ TEST(ex06,test1)
 	int result=num.getValue();
 	EXPECT_EQ(2,result);
 }
+TEST(ex06,test2)
+{
+	Prime num;
+	num.setValue(21)
+	bool result=num.isValid();
+	EXPECT_EQ(false,result);
+}
+TEST(ex06,test3)
+{
+	Prime num;
+	num.setValue(193)
+	bool result=num.isValid();
+	EXPECT_EQ(true,result);
+}
+TEST(ex06,test3)
+{
+	Prime num1,num2;
+	num1.setValue(193)
+	num2=num1.nextPrime();
+	int result=num2.getValue();
+	EXPECT_EQ(197,result);
+}

40f22585bdcac547ef10b31cacc6b82125c9f223
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2020-11-09 17:44:39 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,28 +6,28 @@
 TEST(ex06,test1)
 {
 	Prime num;
-	num.setValue(2)
+	num.setValue(2);
 	int result=num.getValue();
 	EXPECT_EQ(2,result);
 }
 TEST(ex06,test2)
 {
 	Prime num;
-	num.setValue(21)
+	num.setValue(21);
 	bool result=num.isValid();
 	EXPECT_EQ(false,result);
 }
 TEST(ex06,test3)
 {
 	Prime num;
-	num.setValue(193)
+	num.setValue(193);
 	bool result=num.isValid();
 	EXPECT_EQ(true,result);
 }
 TEST(ex06,test4)
 {
 	Prime num1,num2;
-	num1.setValue(193)
+	num1.setValue(193);
 	num2=num1.nextPrime();
 	int result=num2.getValue();
 	EXPECT_EQ(197,result);
@@ -43,8 +43,8 @@ TEST(ex06,test5)
 TEST(ex06,test6)
 {
 	Prime num1,num2;
-	num1.setValue(11)
-	num2.setValue(13)
+	num1.setValue(11);
+	num2.setValue(13);
 	int result=num1.countBetween(num2);
 	EXPECT_EQ(0,result);
 }
